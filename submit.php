1133215d375e903c08f9505b731e5c12398ca7d4
Christian Cassani <32162155+chricassani@users.noreply.github.com>
2018-07-25 20:05:48 +0200

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,38 +1,13 @@
 <?php
-    if(isset($_POST['submit']))
-    {
+   
         $name = $_POST['name']; // Get Name value from HTML Form
         $email_id = $_POST['email']; // Get Email Value
-        $mobile_no = $_POST['mobile']; // Get Mobile No
         $msg = $_POST['message']; // Get Message Value
          
         $to = "ch.cassani@gmail.com"; // You can change here your Email
         $subject = "'$name' has been sent a mail"; // This is your subject
+        $body= "this is test email";
 
-$message ="
-        <html>
-            <body>
-                <table style='width:600px;'>
-                    <tbody>
-                        <tr>
-                            <td style='width:150px'><strong>Name: </strong></td>
-                            <td style='width:400px'>$name</td>
-                        </tr>
-                        <tr>
-                            <td style='width:150px'><strong>Email ID: </strong></td>
-                            <td style='width:400px'>$email_id</td>
-                        </tr>
-                        <tr>
-                            <td style='width:150px'><strong>Mobile No: </strong></td>
-                            <td style='width:400px'>$mobile_no</td>
-                        </tr>
-                        <tr>
-                            <td style='width:150px'><strong>Message: </strong></td>
-                            <td style='width:400px'>$msg</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </body>
-        </html>
-        ";
+mail ($to,$subject,$body);
+echo "Message sent !" ;
        
